f4a647ad263dc5253af35281219c1c469cefe352
Rajbir Deb Mishra <rajbir18mishra@gmail.com>
2025-07-11 01:04:14 +0530

    switched from bcrypt to bcryptjs for compatibility with Render

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,7 +1,7 @@
 import userModel from "../models/userModel.js"
 import transactionModel from "../models/transactionModel.js"
 import razorpay from 'razorpay';
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken'
 
 
